75714e75a4d5409e686c782573b353b00b7eb7d4
James Michael Adoremos <adoremos162@gmail.com>
2020-11-07 18:22:40 +0000

    Added snippets for cfn-lint

---
+++ res/cfn-lint/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install cfn-python-lint
+pip install cfn-lint
+
+# Install pydot for VSCode CloudFormation Linter
+# extension used to preview the templates as
+# graphs.
+pip install pydot
