9fb16699b84b9f9336e4b201ca75eae4a1906f5e
jmadoremos <21135564+jmadoremos@users.noreply.github.com>
2020-11-07 18:42:50 +0000

    Updated to include CF template generation

--- res/cfn-lint/execute.sh
+++ res/cfn-lint/execute.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+# ! Execute at the root directory of this
+# ! git repository. Call this using
+# ! `./res/cfn-lint/execute.sh`.
+
+# Create/update CloudFormation template
+cd ./res/initialize-serverless
+serverless package --stage dev
+cd ../../
+serverless package --stage local
+
 # Execute CloudFormation lint using Python3
 cfn-lint
